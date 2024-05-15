259e029fd613c4e6c2fcb8a4c3fa1d75a5ccee97
choi <caihua90@qq.com>
2024-05-15 15:59:08 +0800

    initial commit

--- client/index.js
+++ client/index.js
@@ -1,14 +1,21 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const MerkleTree = require("../utils/MerkleTree");
+const niceList = require("../utils/niceList.json");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  // TODO: how do we prove to the server we're on the nice list?
+  // from 0 to length-1
+  const i = Math.floor(Math.random() * niceList.length);
+  const name = niceList[i];
+  console.log(name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: merkleTree.getProof(i),
+    name,
   });
 
   console.log({ gift });
